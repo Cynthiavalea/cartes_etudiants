37068619542b96634e32bae3248cae7995853075
Cynthia <cynthiavalea@gmail.com>
2022-06-14 19:40:57 +0000

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+<form action="{{ route('profile') }}" method="get" class="uk-align-center">
+    <input type="text" name="nom" placeholder="Votre nom">
+    <input type="text" name="prenom" placeholder="Votre prenom">
+    <input type="submit" value="Pret">
+</form>
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,3 @@
+<h1>
+    Bonsoir tout le monde
+</h1>
