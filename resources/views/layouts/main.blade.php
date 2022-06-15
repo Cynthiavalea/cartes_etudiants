8e4efe45ccecfb4aade09cea405c2e1d9c969888
Kidchris <christianjosaphat0@gmail.com>
2022-06-15 11:02:51 +0000

    continuation sur les layouts (template)

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,97 @@
+<link rel="stylesheet" href="{{asset('css/style1.css')}}" />
+<link rel="stylesheet" href="{{asset('css/all.min.css')}}" />
+<link rel="icon" href="{{asset('univbobo.png')}}" type="image/png">
+
+
+<body class="crm_body_bg">
+    <nav class="sidebar">
+        <div class="logo d-flex justify-content-between">
+            <a href="index.html"><img src="../public/univbobo.png" alt="">
+                <p style="text-align:center ;">Universite Nazi Boni</p>
+            </a>
+            <div class="sidebar_close_icon d-lg-none">
+                <i class="ti-close"></i>
+            </div>
+        </div>
+        <ul id="sidebar_menu">
+            <li class="mm-active">
+                <a class="has-arrow" href="index.html#" aria-expanded="false">
+
+                    <i class="fa-solid fa-house"></i>
+                    <span>Acceuil</span>
+                </a>
+            </li>
+            <li class="">
+                <a class="has-arrow" href="index.html#" aria-expanded="false">
+                    <i class="fa-solid fa-users-line"></i>
+                    <span>Etudiants</span>
+                </a>
+            </li>
+            <li class="">
+                <a class="has-arrow" href="index.html#" aria-expanded="false">
+                    <i class="fa-solid fa-headset"></i>
+                    <span>Secrétaire</span>
+                </a>
+            </li>
+            </li>
+            <li class="">
+                <a class="has-arrow" href="index.html#" aria-expanded="false">
+                    <i class="fa-solid fa-user"></i>
+                    <span>Profile</span>
+                </a>
+            </li>
+            <li class="">
+                <a class="has-arrow" href="index.html#" aria-expanded="false">
+                    <i class="fa-solid fa-gear"></i>
+                    <span>Paramètre</span>
+                </a>
+        </ul>
+    </nav>
+
+
+    <section class="main_content dashboard_part">
+
+        <div class="container-fluid g-0">
+            <div class="row">
+                <div class="col-lg-12 p-0">
+                    <div class="header_iner d-flex justify-content-between align-items-center">
+                        <div class="sidebar_icon d-lg-none">
+                            <i class="ti-menu"></i>
+                        </div>
+                        <div class="serach_field-area">
+                            <div class="search_inner">
+                                <div class="col">
+                                    <div class="row">
+                                        <p>@Kriskid</p>
+                                        <p>Directeur</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="header_right d-flex justify-content-between align-items-center">
+                            <div class="header_notification_warp d-flex align-items-center">
+
+                            </div>
+                            <div class="profile_info">
+                                <div><img src="img/client_img.png" alt="#"> Connecté <br> <br></div>
+                                <div>
+
+                                    <button type="button" class="btn btn-primary" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+                                        Ajouter etudiant
+                                    </button>
+                                    <button type="button" class="btn btn-primary" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+                                        Ajouter secretaire
+                                    </button>
+                                    </button>
+                                    <button type="button" class="btn btn-primary" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+                                        Liste <i class="fa-solid fa-angle-right"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
