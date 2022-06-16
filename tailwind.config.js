f0c75f0751a94552583e81c9618a166ba1445279
Kidchris <christianjosaphat0@gmail.com>
2022-06-16 09:22:44 +0000

    inclusion de tailwind et listing des secretaires

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
+<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
+=======
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+>>>>>>> 64659834 (inclusion de tailwind et listing des secretaires)
   theme: {
     extend: {},
   },
   plugins: [],
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 64659834 (inclusion de tailwind et listing des secretaires)
